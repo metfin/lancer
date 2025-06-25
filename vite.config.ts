82250d60ca94f01e2f368cabf9182161a02afdeb
heywinit <heywinit@gmail.com>
2025-06-26 00:48:42 +0530

    project setup

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, "public/popup.html"),
+        content: path.resolve(__dirname, "src/content.ts"),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === "content"
+            ? "[name].js"
+            : "assets/[name]-[hash].js";
+        },
+      },
+    },
+  },
+  define: {
+    global: "globalThis",
+  },
+});
